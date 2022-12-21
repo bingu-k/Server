13d76e386aa30a24bc86e40094c3ce881bf27078
byeunghyeun kim <bingu4761@naver.com>
2022-12-21 18:51:01 +0900

    Success Serialize4

---
+++ 11_Serialize/Server/DummyClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using ServerCore;
+
+namespace DummyClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            Connector connector = new Connector();
+            
+            while (true)
+            {
+                try
+                {
+                    connector.Connect(endPoint, () => { return new ServerSession(); });
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ 11_Serialize/Server/Server/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ServerCore;
+using System.Net;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        static Listener _listener = new Listener();
+
+        static void Main(string[] args)
+        {
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            _listener.Init(endPoint, () => { return new ClientSession(); });
+            Console.WriteLine("Listening...");
+
+            while (true)
+            {
+                Thread.Sleep(0);
+            }
+        }
+    }
+}
